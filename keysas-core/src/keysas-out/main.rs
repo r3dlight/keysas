26e83689e4c576df6855cb052413dfe4770ff174
Stephane Neveu <stephane.neveu@ssi.gouv.fr>
2023-03-06 14:19:52 +0100

    Test logic to write output files

--- keysas-core/src/keysas-out/main.rs
+++ keysas-core/src/keysas-out/main.rs
@@ -308,7 +308,6 @@ fn output_files(files: Vec<FileData>, conf: &Configuration) {
             && !f.md.is_toobig
             && f.md.is_type_allowed
             && f.md.av_pass
-            && f.md.yara_pass
             && (f.md.yara_pass || (!f.md.yara_pass && !conf.yara_clean))
         {
             // Output file
