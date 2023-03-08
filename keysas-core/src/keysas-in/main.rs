859953d04485762c26113e460477a9896b252005
Stephane Neveu <stephane.neveu@ssi.gouv.fr>
2023-03-08 10:42:55 +0100

    cargo fmt

--- keysas-core/src/keysas-in/main.rs
+++ keysas-core/src/keysas-in/main.rs
@@ -170,7 +170,6 @@ fn is_corrupted(file: PathBuf) -> bool {
             None => {
                 let ioerror = append_ext("ioerror", file);
                 ioerror.exists() && ioerror.is_file()
-
             }
         }
     } else {
