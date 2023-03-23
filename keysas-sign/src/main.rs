9401a5fb40544be6ceec44f61e04247ea857e893
Stephane Neveu <stephane.neveu@ssi.gouv.fr>
2023-03-23 08:40:54 +0100

    Add keypair creation with oqs & dalek

--- keysas-sign/src/main.rs
+++ keysas-sign/src/main.rs
@@ -7,34 +7,44 @@
  * The code for keysas-sign binary.
  */
 
-use anyhow::anyhow;
+pub use anyhow::{anyhow, Context, Result};
 use clap::{crate_version, Arg, ArgAction, Command};
-use openssl::hash::MessageDigest;
-use openssl::nid::Nid;
-use openssl::pkey::PKey;
 use openssl::x509::X509;
-use openssl::x509::X509NameBuilder;
-use openssl::x509::X509ReqBuilder;
-use std::error::Error;
+use oqs::sig::PublicKey;
+use oqs::sig::SecretKey;
+use oqs::sig::Sig;
+use pkcs8::pkcs5::pbes2;
+use pkcs8::LineEnding;
+use pkcs8::ObjectIdentifier;
+use pkcs8::PrivateKeyInfo;
 use std::fs::File;
 use std::io::prelude::*;
 mod errors;
-use crate::errors::*;
-extern crate libc;
-extern crate udev;
 use std::str;
+// Downgrade for ed25519-dalek
+use ed25519_dalek::Keypair;
+use hex_literal::hex;
+use oqs::sig::Algorithm;
+use rand_dl::rngs::OsRng;
 
 const FILE_PRIV_PATH: &str = "/etc/keysas/file-sign-priv.pem";
 const FILE_CERT_PATH: &str = "/etc/keysas/file-sign-cert.pem";
+const FILE_PRIV_PQ_PATH: &str = "/etc/keysas/file-sign-pq-priv.pem";
+const FILE_CERT_PQ_PATH: &str = "/etc/keysas/file-sign-pq-cert.pem";
 const USB_CERT_PATH: &str = "/etc/keysas/usb-ca-cert.pem";
 
 /// Store command arguments
 struct Config {
-    generate:  bool,   // True for the generate command
-    load:      bool,   // True for the load command
-    name:  String, // Organisation name to put in the certificate request
+    generate: bool,    // True for the generate command
+    load: bool,        // True for the load command
+    name: String,      // Organisation name to put in the certificate request
     cert_type: String, // Certificate type being loaded
-    cert:      String  // Certificate value
+    cert: String,      // Certificate value
+}
+
+enum KeyType {
+    CLASSIC,
+    PQ,
 }
 
 /// Parse command arguments
@@ -55,9 +65,9 @@ fn command_args() -> Config {
             .help("Generate a private for signing purpose (Default is false).")
             .default_value("false")
             .action(ArgAction::SetTrue)
-            .requires("orgname")
-            .requires("orgunit")
-            .requires("country")
+            //.requires("orgname")
+            //.requires("orgunit")
+            //.requires("country")
             .conflicts_with("load")
     )
     .arg(
@@ -73,72 +83,112 @@ fn command_args() -> Config {
     )
     .arg(
         Arg::new("name")
+            .short('n')
             .long("name")
             .value_name("name")
             .help("Name for the station certificate")
             .default_value("")
+            .action(clap::ArgAction::Set)
     )
     .arg(
         Arg::new("certtype")
+            .short('t')
             .long("certtype")
             .value_name("certtype")
             .help("[file|usb]: file is the station file signature certificate, usb is the CA certificate")
             .default_value("")
+            .action(clap::ArgAction::Set)
     )
     .arg(
         Arg::new("cert")
+            .short('c')
             .long("cert")
             .value_name("cert")
             .help("Content of the certificate in PEM format")
             .default_value("")
+            .action(clap::ArgAction::Set)
     )
     .get_matches();
 
     Config {
-        generate:  matches.get_flag("generate"),
-        load:      matches.get_flag("load"),
-        name:  matches.get_one::<String>("orgname").unwrap().to_string(),
+        generate: matches.get_flag("generate"),
+        load: matches.get_flag("load"),
+        name: matches.get_one::<String>("name").unwrap().to_string(),
         cert_type: matches.get_one::<String>("certtype").unwrap().to_string(),
-        cert:      matches.get_one::<String>("cert").unwrap().to_string()
+        cert: matches.get_one::<String>("cert").unwrap().to_string(),
     }
+}
+
+/// Store a keypair in a PKCS8 file without password
+fn store_keypair(prk: &[u8], pbk: &[u8], kind: KeyType, path: &String) -> Result<()> {
+    let params = match pbes2::Parameters::pbkdf2_sha256_aes256cbc(
+        2048,
+        &hex!("79d982e70df91a88"),
+        &hex!("b2d02d78b2efd9dff694cf8e0af40925"),
+    ) {
+        Ok(p) => p,
+        Err(e) => {
+            return Err(anyhow!("Failed to generate pkcs5 parameters: {e}"));
+        }
+    };
+    let (label, oid) = match kind {
+        KeyType::CLASSIC => ("ED25519", ObjectIdentifier::new("1.3.101.112").unwrap()),
+        KeyType::PQ => (
+            "Dilithium5",
+            ObjectIdentifier::new("1.3.6.1.4.1.2.267.3").unwrap(),
+        ),
+    };
+
+    let pk_info = PrivateKeyInfo {
+        algorithm: pkcs8::AlgorithmIdentifierRef {
+            oid,
+            parameters: None,
+        },
+        private_key: prk,
+        public_key: Some(pbk),
+    };
+    // Caution here: Storing into pkcs8 format using an clear password to be able to write_pem_file
+    let pk_encrypted = pk_info.encrypt_with_params(params, "Keysas007").unwrap();
+    pk_encrypted.write_pem_file(path, label, LineEnding::LF)?;
+
+    Ok(())
+}
+
+fn gen_ed25519() -> Result<Keypair> {
+    let mut csprng = OsRng {};
+    let keypair: Keypair = Keypair::generate(&mut csprng);
+    store_keypair(
+        &keypair.secret.to_bytes(),
+        &keypair.public.to_bytes(),
+        KeyType::CLASSIC,
+        &FILE_PRIV_PATH.to_string(),
+    )?;
+    Ok(keypair)
+}
+
+fn gen_pq() -> Result<(PublicKey, SecretKey)> {
+    // Create the signing Dilithium L5 keypair
+    let pq_scheme = Sig::new(Algorithm::Dilithium5)?;
+    let (pk, sk) = pq_scheme.keypair()?;
+    store_keypair(
+        &sk.as_ref(),
+        &pk.as_ref(),
+        KeyType::PQ,
+        &FILE_PRIV_PQ_PATH.to_string(),
+    )?;
+    Ok((pk, sk))
 }
 
 /// Generate a new key and certification request
 /// The private key is saved to a new file at privkey_path
 /// The certificate request is a PEM-encoded PKCS#10 structure
-fn generate_signing_keypair(config: &Config) -> Result<Vec<u8>, Box<dyn Error>>{
-    // Generate the private key
-    let key = PKey::generate_ed25519()?;
+fn generate_signing_keypair(config: &Config) -> Result<()> {
+    // Generate the private keys
+    let _ec_key = gen_ed25519()?;
+    let _pq_key = gen_pq()?;
+    // Build the csr now
 
-    // Store the private key in a PEM encoded PKCS8 file
-    let buf = key.private_key_to_pem_pkcs8()?;
-    let mut output = File::create(FILE_PRIV_PATH)?;
-    output.write_all(&buf)?;
-
-    // Generate the certification request
-    let mut builder = X509ReqBuilder::new()?;
-    
-    // Set version
-    builder.set_version(2)?;
-
-    // Set subject name
-    let mut name_builder = X509NameBuilder::new()?;
-    name_builder.append_entry_by_nid(Nid::COMMONNAME, &config.name)?;
-    let name = name_builder.build();
-    builder.set_subject_name(name.as_ref())?;
-
-    // Set public key
-    let raw_pub = key.raw_public_key()?;
-    let pub_key = PKey::public_key_from_raw_bytes(&raw_pub, key.id())?;
-    builder.set_pubkey(pub_key.as_ref())?;
-
-    // Set request
-    builder.sign(&key, MessageDigest::null())?;
-
-    let req = builder.build();
-    let out = req.to_pem()?;
-
-    Ok(out)
+    Ok(())
 }
 
 fn save_certificate(cert_type: &String, cert: &String) -> Result<()> {
@@ -162,6 +212,9 @@ fn save_certificate(cert_type: &String, cert: &String) -> Result<()> {
 
 fn main() -> Result<()> {
     let config = command_args();
+
+    // Important load oqs:
+    oqs::init();
 
     if config.generate {
         let req = match generate_signing_keypair(&config) {
