2b256b5f104ed454f09ef84d8cf6d4028e6850c4
Stephane Neveu <stephane.neveu@ssi.gouv.fr>
2023-05-22 13:24:36 +0200

    UX: color update on homepage + remove useless method

--- keysas-admin/src/views/AboutView.vue
+++ keysas-admin/src/views/AboutView.vue
@@ -13,10 +13,6 @@ export default {
     return {
     }
   },
-
-  methods: {
-
-  }
 }
 
 </script>
@@ -24,6 +20,7 @@ export default {
 <template>
   <NavBar />
   <h2>Version 2.0.0</h2>
+  <h2>Please visit <a href="#https://keysas.fr" class="text-primary">keysas.fr</a> to learn more </h2>
   <h3>Copyright Keysas 2022-2023.</h3>
 </template>
 
