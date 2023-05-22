3542345bd01ed8917d2e897ae2c433449b1fa80a
Stephane Neveu <stephane.neveu@ssi.gouv.fr>
2023-05-22 13:24:36 +0200

    UX: color update on homepage + remove useless method

--- keysas-admin/src/views/HomeView.vue
+++ keysas-admin/src/views/HomeView.vue
@@ -7,7 +7,7 @@
     <h1 class="text-info fw-bold bi bi-magic"> QUICK START</h1>
     <br />
     <ul class="doc">
-      <li><b>SSH configuration</b></li>
+      <li><b class="text-info">SSH configuration</b></li>
       <ul>
         <li>First, start by <b>creating</b> a ED25519 <b>private key</b> and the associated <b>public key</b> on your
           computer<br />
@@ -20,7 +20,7 @@
         <li class="doc">Then, <b>set the path</b> of both keys in the <b>"Admin configuration->SSH configuration"</b> menu</li>
       </ul>
       <br>
-      <li><b>Generate a IKPQPKI</b></li>
+      <li><b class="text-info">Generate a IKPQPKI</b></li>
       <ul>
         <li>To be able to sign your outgoing USB devices and to enroll new Keysas stations, you need to create a <b>IKPQPKI</b> 
           <br>(<b>I</b>ncredible <b>K</b>eysas (Hybrid) <b>P</b>ost-<b>Q</b>uantum <b>P</b>ublic <b>K</b>ey <b>I</b>nfrastucture) &#x1F601;</li>
@@ -29,24 +29,22 @@
         <li>Be patient, this may take a while &#x2615;</li>
       </ul>
       <br>
-      <li><b>Enroll you Keysas stations</b></li>
+      <li><b class="text-info">Enroll you Keysas stations</b></li>
       <ul>
         <li>You can now start <b>adding</b> new keysas stations in the <b>"Add a new Keysas"</b> menu
           providing a name and an IP address</li>
         <li>When done, <b>export</b> the public key by clicking the <b>"Export SSH pubkey"</b> button for each station added</li>
       </ul>
       <br>
-      <li><b>Sign your outgoing USB keys</b></li>
+      <li><b class="text-info">Sign your outgoing USB keys</b></li>
       <ul>
         <li>You can now start signing at least one USB device in "<b>Admin configuration</b>-><b>USB Signing</b>"</li>
         <li>Type the password provided during your IKPQPKI creation and plug the USB key</li>
         <li>Be patient, this may take a while &#x2615;</li>
       </ul>
       <br>
-      <li><b>You're now ready to go !</b></li>
+      <li><b class="text-info">You're now ready to go !</b></li>
     </ul>
-    <br><br><span class="website">Please visit <a href="#https://keysas.fr" class="text-primary">keysas.fr</a> to learn
-      more !</span>
   </div>
 </template>
 
