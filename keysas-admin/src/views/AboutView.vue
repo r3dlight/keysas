0828d75f0897c137819b29f535f232e557110955
Stephane Neveu <stephane.neveu@ssi.gouv.fr>
2023-08-04 14:06:33 +0200

    Bump version to 2.1

--- keysas-admin/src/views/AboutView.vue
+++ keysas-admin/src/views/AboutView.vue
@@ -19,7 +19,7 @@ export default {
 
 <template>
   <NavBar />
-  <h2>Version 2.0.0</h2>
+  <h2>Version 2.1</h2>
   <h2>Please visit <a href="#https://keysas.fr" class="text-primary">keysas.fr</a> to learn more </h2>
   <h3>Copyright Keysas 2022-2023.</h3>
 </template>
